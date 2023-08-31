9e95e1709e6fcd2943553afdb9a2b266e28f6ad8
Xamman27 <xamman27@gmail.com>
2023-08-31 22:24:47 +0400

    done

--- demo_qa_tests/data/registration_page.py
+++ demo_qa_tests/data/registration_page.py
@@ -1,6 +1,8 @@
 from selene import browser, be, have, command
 from demo_qa_tests.data.users import Users
 from demo_qa_tests.data.path import image_path
+
+
 class RegistrationPage():
     def __init__(self):
         self.input_first_name = browser.element('#firstName')
@@ -9,7 +11,7 @@ class RegistrationPage():
         self.input_phone_number = browser.element('#userNumber')
         self.input_birthday = browser.element('#dateOfBirthInput')
         self.input_subject = browser.element('#subjectsInput')
-        self.radio_button_hobies = browser.all
+        self.radio_button_hobbies = browser.all
         self.upload_picture = browser.element('#uploadPicture')
         self.input_current_address = browser.element('#currentAddress')
         self.input_state = browser.element('#state')
@@ -17,7 +19,7 @@ class RegistrationPage():
         self.submit_button = browser.element('#submit')
 
         self.result_student_name = browser.element('//tbody/tr[1]/td[2]')
-        self.result_mail = browser.element('//tbody/tr[2]/td[2]')
+        self.result_email = browser.element('//tbody/tr[2]/td[2]')
         self.result_gender = browser.element('//tbody/tr[3]/td[2]')
         self.result_phone = browser.element('//tbody/tr[4]/td[2]')
         self.result_birthday = browser.element('//tbody/tr[5]/td[2]')
@@ -46,29 +48,31 @@ class RegistrationPage():
         self.input_subject.click().send_keys(student.subject).press_enter()
         browser.element('[for="hobbies-checkbox-1"]').perform(command.js.scroll_into_view).click()
         self.upload_picture.set_value(image_path)
-        self.input_current_address.should(be.blank).type(student.current_address)
+        self.input_current_address.type(student.current_address)
+        self.input_state.perform(command.js.scroll_into_view)
+        self.input_state.click()
+        browser.all('[id^=react-select][id*=option]').element_by(
+            have.exact_text(student.state)
+        ).click()
         self.input_city.perform(command.js.scroll_into_view)
-        self.input_state.type(student.state).press_enter()
-        self.input_city.type(student.city).press_enter()
-        self.submit_button.should(be.visible).click()
-        # browser.element('#state').click()
-        # browser.all('[id^=react-select][id*=option]').element_by(
-        #     have.exact_text(student.state)
-        # ).click()
-        #
+        self.input_city.click()
+        browser.all('[id^=react-select][id*=option]').element_by(
+            have.exact_text(student.city)
+        ).click()
+        self.submit_button.click()
 
-        # browser.element('#city').perform(command.js.scroll_into_view)
-        # browser.element('#city').click()
-        # browser.all('[id^=react-select][id*=option]').element_by(
-        #     have.exact_text(student.city)
-        # ).click()
 
-        # self.input_state.should(be.visible).type(student.state).press_enter()
-        # self.input_city.should(be.clickable).type(student.city).press_enter()
 
     def chech_registr(self, student=Users):
         self.result_student_name.should(have.text(f'{student.first_name} {student.last_name}'))
-        self.result_mail.should(have.text(student.mail))
+        self.result_email.should(have.text(student.email))
+        self.result_gender.should(have.text(student.gender))
+        self.result_phone.should(have.text(student.phone_number))
+        self.result_subject.should(have.text(student.subject))
+        self.result_hobbies.should(have.text(student.hobbies))
+        self.result_image.should(have.text('image.jpeg'))
+        self.result_city.should(have.text(student.current_address))
+        self.result_address.should(have.text(f'{student.state} {student.city}'))
 
 
 
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,20 @@
 import pytest
 from selene import browser
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture(autouse=True)
 def browser_settings():
     browser.config.base_url = 'https://demoqa.com/'
+    driver_options = webdriver.ChromeOptions()
+    browser.config.type_by_js = True
+    browser.config.driver_options = driver_options
+    browser.config.driver = webdriver.Chrome(
+        service=ChromeService(executable_path=ChromeDriverManager().install()),
+        options=driver_options)
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    browser.config.type_by_js = True
     yield
     browser.quit()
