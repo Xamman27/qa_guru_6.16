84d8bb8edf7962e95dc8f22519887a6812df33a7
Xamman27 <xamman27@gmail.com>
2023-09-15 15:53:12 +0400

    new task for jenkins

--- tests/test_for_demoqa.py
+++ tests/test_for_demoqa.py
@@ -1,3 +1,5 @@
+import allure
+
 from demo_qa_tests.data.users import Users
 from demo_qa_tests.data.path import image_path
 from demo_qa_tests.data.registration_page import RegistrationPage
@@ -9,8 +11,11 @@ student = Users(
 )
 reg_page = RegistrationPage()
 
-
+@allure.title("Successful registration")
 def test_simple():
-    reg_page.open()
-    reg_page.register(student)
-    reg_page.chech_registr(student)
+    with allure.step("Open registration form"):
+        reg_page.open()
+    with allure.step("Fill form"):
+        reg_page.register(student)
+    with allure.step("Assert form results"):
+        reg_page.chech_registr(student)
