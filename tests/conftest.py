b7c8d8f953b92fb283b533931de5886216b66d1c
Xamman27 <xamman27@gmail.com>
2023-09-16 23:37:41 +0400

    refactor conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,14 +8,6 @@ from dotenv import load_dotenv
 
 from utils import attach
 
-DEFAULT_BROWSER_VERSION = "100.0"
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        '--browser_version',
-        default='100.0'
-    )
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -25,7 +17,7 @@ def load_env():
 
 @pytest.fixture(scope='function')
 def setup_browser():
-    browser_version = DEFAULT_BROWSER_VERSION
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -44,7 +36,7 @@ def setup_browser():
         command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
-    browser = Browser(Config(driver))
+    browser = Browser(Config())
 
     yield browser
 
